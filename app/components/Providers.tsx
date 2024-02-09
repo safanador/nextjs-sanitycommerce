458134118d92712daa0751b13623bfcf07e86fdc
Sergio Afanador <sergioafanador102@gmail.com>
2024-02-09 17:09:46 -0500

    everything should work now

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -5,8 +5,8 @@ import { CartProvider as USCProvider } from "use-shopping-cart";
 export default function CartProvider({children}:{children:ReactNode}){
     return(
         <USCProvider mode="payment" cartMode="client-only" stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-        successUrl="http://localhost:3000/stripe/success"
-        cancelUrl="http://localhost:3000/stripe/error"
+        successUrl="https://nextjs-sanitycommerce.vercel.app/stripe/success"
+        cancelUrl="https://nextjs-sanitycommerce.vercel.app/stripe/error"
         currency="USD"
         billingAddressCollection={false}
         shouldPersist={true}
--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -23,6 +23,7 @@ async function getData(slug:string){
     return data;
 }
 
+export const dynamic = "force-dynamic";
 
 export default async function ProductPage({params}:{params:{slug:string}}){
     const data:fullProduct = await getData(params.slug)
