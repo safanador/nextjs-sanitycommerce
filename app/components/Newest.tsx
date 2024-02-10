3d91326d40e8688e81f5c2ffe90a75da5dbff8b0
Sergio Afanador <sergioafanador102@gmail.com>
2024-02-10 11:18:34 -0500

    responsiveness

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -31,25 +31,29 @@ export default async function Newest(){
                         See All{" "}<span><ArrowRight/></span>
                     </Link>
                 </div>
-                <div className="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div className="mt-6 grid grid-cols-2 gap-x-1 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {data.map((product)=>(
                         <div key={product._id} className="group relative">
-                            <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+                            
+                            <Link href={`/product/${product.slug}`}>
+                                <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
                                     <Image
                                     alt="Product image"
                                     src={product.imageUrl}
                                     className="w-full h-full object-cover object-center lg:h-full lg:w-full"
                                     width={300}
                                     height={300}/>
-                            </div>
-                            <div className="mt-4 flex justify-between">
+                                </div>
+                            </Link>
+                            
+                            <div className="mt-4 grid grid-cols-1 md:flex justify-between">
                                 <div>
-                                    <h3 className="text-sm text-gray-700">
+                                    <h3 className="text-sm text-gray-950 line-clamp-2">
                                         <Link href={`/product/${product.slug}`}>
                                             {product.name}
                                         </Link>
                                     </h3>
-                                    <p className="mt-1 text-sm text-gray-500">{product.categoryName}</p>         
+                                    <p className="mt-1 text-sm text-gray-500 ">{product.categoryName}</p>         
                                 </div>        
                                 <p
                                 className="text-sm font-medium text-gray-90">${product.price}</p>          
